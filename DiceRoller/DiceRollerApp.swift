18a71548bbb9662d9a04129d5f513396046ef2fd
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-12-28 01:16:21 +0530

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Sandeep Singh on 28/12/20.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
